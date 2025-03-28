9257e793ab061fd323dbfce291e5841724037203
Leonie Miege <leonie.miege@gmail.com>
2025-03-28 12:33:33 +0100

    Début étape 3 : afficher la température courante de la ville

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // API variables
 const nominatimAPI = "https://nominatim.openstreetmap.org";
+const openMeteoAPI = "https://api.open-meteo.com";
+
 
 // DOM Elements
 const cityInput = document.querySelector("#cityInput");
 const searchButton = document.querySelector("#searchButton");
 const cityElement = document.querySelector("#city");
 const gpsElement = document.querySelector("#gps");
-// const temperatureElement = document.querySelector("#temperature");
-// const detailsElement = document.querySelector("#details");
+const temperatureElement = document.querySelector("#temperature");
+const detailsElement = document.querySelector("#details");
 
 // Declaration of functions
 async function fetchCoordinates(city) {
@@ -16,21 +18,33 @@ async function fetchCoordinates(city) {
         const data = await response.json();
 
         if (data.length === 0) {
-            throw new Error('Pas de coordonnées trouvées pour la ville fournie');
+            throw new Error('Ville non trouvée');
         }
 
         const coordinates = {
             name: data[0].name,
             lat: Number(data[0].lat),
-            lng: Number(data[0].lon),
+            lon: Number(data[0].lon),
         }
         return coordinates;
     } catch (error) {
-        console.error('Error fetching coordinates:', error);
-        gpsElement.innerText = 'Impossible de trouver les coordonnées de la ville';
+        console.error('Error fetching coordinates', error);
+        gpsElement.innerText = 'Impossible de récupérer les coordonnées de la ville saisie';
         throw error;
     }
 }
+
+async function fetchWeather() {
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,precipitation,relative_humidity_2m`);
+        const data = await response.json();
+        const temperature = Number(data[0].current.temperature_2m)
+        return temperature
+    } catch (error) {
+        console.error('Error fetching temperature', error);
+    }
+}
+
 async function handleSearch() {
     const city = cityInput.value;
     try {
@@ -38,7 +52,10 @@ async function handleSearch() {
         cityElement.innerText = `${coordinates.name}`;
         gpsElement.innerText = `Coordonnées GPS : 
         ${coordinates.lat.toFixed(7)}
-        ${coordinates.lng.toFixed(7)}`;
+        ${coordinates.lon.toFixed(7)}`;
+        
+        const temperature = await fetchWeather(coordinates.lat, coordinates.lon);
+        temperatureElement.innerText = `${temperature.current.temperature_2m}`
     } catch (error) {
         console.error(error);
     }
